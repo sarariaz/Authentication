6db86a20f5b4713648e5063f2558b7fbf080db90
sara riaz <srq147147@gmail.com>
2019-09-16 12:37:51 -0700

    AuthDemo using Passport

--- AuthDemo/app.js
+++ AuthDemo/app.js
@@ -12,14 +12,33 @@ mongoose.connect("mongodb://localhost/auth_demo_app",{useNewUrlParser:true});
 app.set("view engine", "ejs");
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(require("express-session")(
+    {
+        secret: "Rusty is the best dog ", // secret is used to decode the session details
+        resave: false,
+        saveUninitialized: false
+    }
+));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser()); //reading session, taking data from session that os encoded or decoded
 
-// Routes
+
+// =================================================================
+// ROUTESS
+
 app.get("/", function(req,res){
     res.render("home");
 });
 
 app.get("/secret", function(req,res){
     res.render("secret");
+});
+
+/// AUTH ROUTES
+
+// show signup form 
+app.get("/register", function(req,res){
+    res.render("register");
 })
 
 // listen port
